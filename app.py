00a851fd1b91a97f15411dc7f28f50aebab2a4d9
PrincZe <silent_will7@hotmail.com>
2024-03-05 00:20:22 +0800

    stream directly to s3

--- app.py
+++ app.py
@@ -1,55 +1,45 @@
 from flask import Flask, request, jsonify, render_template
 import replicate
-import tempfile
 import os
 import boto3
-from dotenv import load_dotenv
+from io import BytesIO
 
-# Load environment variables from .env file
-load_dotenv()
-
-app = Flask(__name__)
-
-# Access environment variables for sensitive information
 aws_access_key = os.environ.get("AWS_ACCESS_KEY")
 aws_secret_key = os.environ.get("AWS_SECRET_KEY")
-replicate_api_token = os.environ.get("REPLICATE_API_TOKEN")
-bucket_name = os.environ.get("S3_BUCKET_NAME")
+bucket_name = "realtimeconversationupload"
 
-# Initialize AWS S3 client
 s3 = boto3.client(
     "s3", aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key
 )
 
-# Initialize Replicate client
-model = replicate.Client(api_token=replicate_api_token)
+app = Flask(__name__)
+model = replicate
 
-# render HTML
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
-# Function to transcribe audio using Replicate
+
 @app.route("/process-audio", methods=["POST"])
 def process_audio_data():
+    audio_data = request.files["audio"].read()
+
+    print("Processing audio...")
     try:
-        audio_data = request.files["audio"].read()
+        # Stream audio data to S3 bucket
+        s3_object_key = "audio.wav"
+        s3.put_object(Bucket=bucket_name, Key=s3_object_key, Body=BytesIO(audio_data))
 
-        print("Processing audio...")
-        # Create a temporary file to save the audio data
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp_audio:
-            temp_audio.write(audio_data)
-            temp_audio.flush()
+        # Get S3 object URL
+        s3_object_url = f"https://{bucket_name}.s3.amazonaws.com/{s3_object_key}"
 
-            s3.upload_file(temp_audio.name, bucket_name, os.path.basename(temp_audio.name))
-            temp_audio_uri = f"https://{bucket_name}.s3.amazonaws.com/{os.path.basename(temp_audio.name)}"
-
-        # Run Replicate model for transcription
+        # Run Replicate model on S3 object URL
         output = model.run(
             "vaibhavs10/incredibly-fast-whisper:3ab86df6c8f54c11309d4d1f930ac292bad43ace52d10c80d87eb258b3c9f79c",
             input={
                 "task": "transcribe",
-                "audio": temp_audio_uri,
+                "audio": s3_object_url,
                 "language": "english",
                 "timestamp": "chunk",
                 "batch_size": 64,
@@ -58,50 +48,39 @@ def process_audio_data():
         )
 
         results = output["text"]
-
         return jsonify({"transcript": results})
-
     except Exception as e:
         print(f"Error processing audio: {e}")
-        return jsonify({"error": "An error occurred while processing the audio"}), 500
+        return jsonify({"error": str(e)}), 500
 
-# Function to generate suggestion using Replicate
+
 @app.route("/get-suggestion", methods=["POST"])
 def get_suggestion():
-    try:
-        data = request.get_json()  # Parse JSON data from the request
-        transcript = data.get("transcript", "")  # Extract transcript
-        prompt_text = data.get("prompt", "")  # Extract prompt text
+    data = request.get_json()
+    transcript = data.get("transcript", "")
+    prompt_text = data.get("prompt", "")
 
-        prompt = f"""
-        {transcript}
-        ------
-        {prompt_text}
-        """
+    prompt = f"{transcript}\n------\n{prompt_text}"
 
-        suggestion = ""
-        for event in model.stream(
-            "mistralai/mistral-7b-instruct-v0.2",
-            input={
-                "debug": False,
-                "top_k": 50,
-                "top_p": 0.9,
-                "prompt": prompt,
-                "temperature": 0.6,
-                "max_new_tokens": 512,
-                "min_new_tokens": -1,
-                "prompt_template": "<s>[INST] {prompt} [/INST] ",
-                "repetition_penalty": 1.15,
-            },
-        ):
-            suggestion += str(event)  # Accumulate the output
+    suggestion = ""
+    for event in model.stream(
+        "mistralai/mistral-7b-instruct-v0.2",
+        input={
+            "debug": False,
+            "top_k": 50,
+            "top_p": 0.9,
+            "prompt": prompt,
+            "temperature": 0.6,
+            "max_new_tokens": 512,
+            "min_new_tokens": -1,
+            "prompt_template": "<s>[INST] {prompt} [/INST] ",
+            "repetition_penalty": 1.15,
+        },
+    ):
+        suggestion += str(event)
 
-        return jsonify({"suggestion": suggestion})  # Send as JSON response
+    return jsonify({"suggestion": suggestion})
 
-    except Exception as e:
-        print(f"Error generating suggestion: {e}")
-        return jsonify({"error": "An error occurred while generating suggestion"}), 500
 
-# Run the Flask app
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
